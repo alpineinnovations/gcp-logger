1b738c3e397664a209097acf7ea7db63d6f39847
David Nasztanovics <naszta@nooal.com>
2025-08-19 09:05:59 +0200

    The changes prevent multiple WriteHeader calls by:   1. Adding a headerWritten flag to track if headers were already sent   2. Checking this flag before calling the underlying WriteHeader   3. Implementing a Write method that ensures headers are sent with status 200 if not already written   4. Updating StatusCode() to return 200 as default when no status was explicitly set
      This should eliminate the "superfluous response.WriteHeader call" warnings.

--- web/middlewares/logging_response_writer.go
+++ web/middlewares/logging_response_writer.go
@@ -4,18 +4,33 @@ import "net/http"
 
 type LoggingResponseWriter struct {
 	http.ResponseWriter
-	statusCode int
+	statusCode    int
+	headerWritten bool
 }
 
 func NewLoggingResponseWriter(w http.ResponseWriter) *LoggingResponseWriter {
-	return &LoggingResponseWriter{w, 0}
+	return &LoggingResponseWriter{w, 0, false}
 }
 
 func (l *LoggingResponseWriter) WriteHeader(code int) {
+	if l.headerWritten {
+		return
+	}
 	l.statusCode = code
+	l.headerWritten = true
 	l.ResponseWriter.WriteHeader(code)
 }
 
+func (l *LoggingResponseWriter) Write(b []byte) (int, error) {
+	if !l.headerWritten {
+		l.WriteHeader(http.StatusOK)
+	}
+	return l.ResponseWriter.Write(b)
+}
+
 func (l *LoggingResponseWriter) StatusCode() int {
+	if l.statusCode == 0 {
+		return http.StatusOK
+	}
 	return l.statusCode
 }
